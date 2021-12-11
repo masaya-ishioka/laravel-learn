633f505cb64140725d8e0f828f9a4cc5970a0926
masaya.ishioka <penpenmono31@gmail.com>
2021-12-11 10:23:17 +0900

    チュートリアル７終了

--- backend/resources/views/layout.blade.php
+++ backend/resources/views/layout.blade.php
@@ -13,10 +13,30 @@
 	<nav class="my-navbar">
 		<a class="my-navbar-brand" href="/">ToDo App</a>
 	</nav>
+	@if (Auth::check())
+		<span class="my-navbar-item">ようこそ{{ Auth::user()->name }}さん</span>
+		|
+		<a href="#" id="logout" class="my-navbar-item">ログアウト</a>
+		<form id="logout-form" action="{{ route('logout') }}" method="POST" style="display:none">
+			@csrf
+		</form>
+	@else
+		<a class="my-navbar-item" href="{{ route('login') }}">ログイン</a>
+		|
+		<a class="my-navbar-item" href="{{ route('register') }}">会員登録</a>
+	@endif
 </header>
 <main>
 	@yield('content')
 </main>
+@if(Auth::check())
+	<script>
+		document.getElementById('logout').addEventListener('click', function (event) {
+			event.preventDefault();
+			document.getElementById('logout-form').submit();
+		});
+	</script>
+@endif
 @yield('scripts')
 </body>
 </html>
